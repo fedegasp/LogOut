ef2d4ee1d53b498bffe3c5314be82a1bd0f647d5
Federico Gasperini <federico.gasperini@gmail.com>
2020-06-05 09:09:49 +0200

    initial commit, empty project

---
+++ LogOut/LogOut/LogOut.h
@@ -0,0 +1,19 @@
+//
+//  LogOut.h
+//  LogOut
+//
+//  Created by Federico Gasperini on 05/06/2020.
+//  Copyright © 2020 Federico Gasperini. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LogOut.
+FOUNDATION_EXPORT double LogOutVersionNumber;
+
+//! Project version string for LogOut.
+FOUNDATION_EXPORT const unsigned char LogOutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LogOut/PublicHeader.h>
+
+
